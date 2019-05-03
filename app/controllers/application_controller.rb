3575e8cbf3ff7b97b1bb711a9562bec1b388b087
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-05-03 17:24:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < Sinatra::Base
     user = User.find_by(username: params["username"], password: params["password"])
   if user
     session[:user_id] = user.id
-    'Welcome #{session[:user_id]}'
+    'Welcome #{session[:username]}'
     redirect to '/account'
   else
     erb :error
